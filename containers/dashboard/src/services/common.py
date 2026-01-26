563b4b8fd4ac493eea0874798eb31e47a61c9826
Kyell Jennsen <mail@kyelljensen.de>
2026-01-26 03:09:06 +0100

    feat: Add a service import debugging script and initialize a logger in `common.py`.

--- containers/dashboard/src/services/common.py
+++ containers/dashboard/src/services/common.py
@@ -4,3 +4,6 @@ DB_PATH = "/data/birdnet_db/birdnet.sqlite"
 REC_DIR = "/data/recording"
 LOG_DIR = "/var/log/silvasonic"
 STATUS_DIR = "/mnt/data/services/silvasonic/status"
+
+import logging
+logger = logging.getLogger("Dashboard.Services")
